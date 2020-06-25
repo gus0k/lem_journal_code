2ec457db13642e6a4c233ba85151508d551d7e6f
Diego K <gusok@protonmail.com>
2020-06-25 13:25:49 +0200

    Lazy git

--- src/plotting.py
+++ src/plotting.py
@@ -10,13 +10,13 @@ from pathlib import Path
 from src.utils import fix_hist_step_vertical_line_at_end
 
 import matplotlib
-matplotlib.use('pgf'),
-matplotlib.rcParams.update({
-    'pgf.texsystem': 'pdflatex',
-    'font.family': 'serif',
-    'text.usetex': True,
-    'pgf.rcfonts': False,
-}),
+# matplotlib.use('pgf'),
+# matplotlib.rcParams.update({
+    # 'pgf.texsystem': 'pdflatex',
+    # 'font.family': 'serif',
+    # 'text.usetex': True,
+    # 'pgf.rcfonts': False,
+# }),
 
 def process_sim(simpath):
 
@@ -27,13 +27,16 @@ def process_sim(simpath):
         return None
     N, H, D, seed, flat, fd, forcast, cant_bats = params
 
-    with open(simpath + 'players.pkl', 'rb') as fh: players = pickle.load(fh)
-    with open(simpath + 'sim_config.pkl', 'rb') as fh: config = pickle.load(fh)
-    with open(simpath + 'simres_muda.pkl', 'rb') as fh: aftersim_muda = pickle.load(fh)
-    with open(simpath + 'simres_p2p.pkl', 'rb') as fh: aftersim_p2p = pickle.load(fh)
-    with open(simpath + 'nomarket.pkl', 'rb') as fh: nomarket = pickle.load(fh)
-    with open(simpath + 'coop_loop_res.pkl', 'rb') as fh: core_loop = pickle.load(fh)
-    with open(simpath + 'core_payments.pkl', 'rb') as fh: core_payoffs = pickle.load(fh)
+    try:
+        with open(simpath + 'players.pkl', 'rb') as fh: players = pickle.load(fh)
+        with open(simpath + 'sim_config.pkl', 'rb') as fh: config = pickle.load(fh)
+        with open(simpath + 'simres_muda.pkl', 'rb') as fh: aftersim_muda = pickle.load(fh)
+        with open(simpath + 'simres_p2p.pkl', 'rb') as fh: aftersim_p2p = pickle.load(fh)
+        with open(simpath + 'nomarket.pkl', 'rb') as fh: nomarket = pickle.load(fh)
+        with open(simpath + 'coop_loop_res.pkl', 'rb') as fh: core_loop = pickle.load(fh)
+        with open(simpath + 'core_payments.pkl', 'rb') as fh: core_payoffs = pickle.load(fh)
+    except:
+        return None
 
     P = int(N)
     lemsims = [aftersim_muda, aftersim_p2p, nomarket]
@@ -89,7 +92,7 @@ def process_sim(simpath):
 
 
 
-def generate_figures(sims, flat='True', forcast='0'):
+def generate_figures(sims, N, cantbats, flat='True', forcast='0'):
 
     data_cost = [[], [], [], []]
     data_load = [[], [], [], []]
@@ -100,10 +103,14 @@ def generate_figures(sims, flat='True', forcast='0'):
     KEYS_SIM_LONG = ['Cooperative perfect', 'Cooperative iterative', 'Non-coop MUDA', 'Non-coop P2P ']
 
     for ss in sims:
-        X = process_sim(ss)
-        if X is not None:
-            res, par = X
-            if (par[4], par[6]) == (flat, forcast):
+        par = ss.split('/')[-2]
+        par, name = par.split('_')
+        par = par.split('-')
+        if len(par) == 8:
+            if (par[0], par[7], par[4], par[6]) == (N, cantbats, flat, forcast):
+                X = process_sim(ss)
+                if X is not None:
+                    res, par = X
 
                 for i, d_ in enumerate(data):
                     tmp = res[data_keys[i]]
@@ -112,12 +119,16 @@ def generate_figures(sims, flat='True', forcast='0'):
                         d_[j].append(val)
 
 
+    numplayers = '# players {} - '.format(N)
+    numbatteries = '- # batteries {} -'.format(cantbats)
     flat_title = ' - Flat tariff - ' if flat == 'True' else ' - Tou Tariff - '
     forcast_title = ' - No forcast - ' if forcast == '0' else ' - with forcast - '
 
+    title_string = numplayers + numbatteries + flat_title + forcast_title
+
 
     BINS = 20
-    WIDTH = 3.5
+    WIDTH = 10
     FIGSIZE = (WIDTH, WIDTH * 2 / (1 + math.sqrt(5)))
 
     fig_cost, ax = plt.subplots(figsize=FIGSIZE)
@@ -131,7 +142,7 @@ def generate_figures(sims, flat='True', forcast='0'):
     ax.legend()
     ax.set_xlabel('% smaller than to no interaction')
     ax.set_ylabel('Fraction of simulations')
-    ax.set_title('Cost' + flat_title + forcast_title)
+    ax.set_title('Cost' + title_string)
     ax.legend()
     fig_cost.tight_layout()
 
@@ -145,7 +156,7 @@ def generate_figures(sims, flat='True', forcast='0'):
     fix_hist_step_vertical_line_at_end(ax)
     ax.set_xlabel('% smaller than to no interaction')
     ax.set_ylabel('Fraction of simulations')
-    ax.set_title('Energy exchanged with the grid' + flat_title + forcast_title)
+    ax.set_title('Energy exchanged with the grid' + title_string)
     ax.legend()
     fig_load.tight_layout()
 
@@ -153,11 +164,20 @@ def generate_figures(sims, flat='True', forcast='0'):
            
 
 if __name__ == '__main__':
-    sims = [str(s) + '/' for s in Path(SIMULATION_PARAMETERS).glob('*good1')]
+    sims = [str(s) + '/' for s in Path(SIMULATION_PARAMETERS).glob('*complete')]
 
-    combs = [('True', '0'), ('True', '1'), ('False', '0'), ('False', '1')]
-    for flat, forcast in combs:
-        fl, fc = generate_figures(sims, flat, forcast)
-        fl.savefig('img/fl{}{}.pgf'.format(flat, forcast))
-        fc.savefig('img/fc{}{}.pgf'.format(flat, forcast))
+    list_of_keys = set()
+    for ss in sims:
+        par = ss.split('/')[-2]
+        par, name = par.split('_')
+        par = par.split('-')
+        key = (par[0], par[7], par[4], par[6])
+        list_of_keys.add(key)
+    print(list_of_keys)
+
+    # combs = [('True', '0'), ('True', '1'), ('False', '0'), ('False', '1')]
+    for N, cantbats, flat, forcast in list_of_keys:
+        fl, fc = generate_figures(sims, N, cantbats, flat, forcast)
+        fl.savefig('img/fl_{}_{}_{}_{}.png'.format(N, cantbats, flat, forcast))
+        fc.savefig('img/fc_{}_{}_{}_{}.png'.format(N, cantbats, flat, forcast))
 
